f0a3ad18fc4a9aac248fd8cbeb94c2f0b0f326a0
nadinelyab <nadinelyab@gmail.com>
2015-08-02 18:03:39 +0200

    All views for proposals, changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+Proposal.delete_all
+School.delete_all
+
 volunteer = User.create! username: "volunteer", email: "volunteer@user.com", password: "password", password_confirmation: "password", category: "volunteer"
 school = User.create! username: "school", email: "school@user.com", password: "password", password_confirmation: "password", category: "school"
 
-proposal = Proposal.create! teacher_name: "Nadin Ey", teacher_location: "Mexico City", topic: "math", duration: "1 hour", target_age: "12", language: "Spanish", user_id: volunteer
+proposal = Proposal.create! teacher_name: "Nadin Ey", teacher_location: "Mexico City", topic: "math", duration: "1 hour", target_age: "12", language: "Spanish", user_id: volunteer,
 description: "A fun introduction to fractals!"
 
 school = School.create! school_name: "South Anchorage High School", location: "Anchorage", needs: "Spanish Teacher", students_age: "15-18", user_id: school
