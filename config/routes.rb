525c1b90683a28645b5e5d877735d831888f6453
nadinelyab <nadinelyab@gmail.com>
2015-08-02 16:49:18 +0200

    Added RESTful routes for proposals and schools

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :proposals
+
+  resources :schools
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
