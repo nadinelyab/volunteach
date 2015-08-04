b9db60e8f281643a9d7cfb0aff2e9885ae408a39
nadinelyab <nadinelyab@gmail.com>
2015-08-04 09:35:55 +0200

    Changing views for messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@proposals = Proposal.where(user: @user)
 		@schools = School.where(user: @user)
-		@messages_received = Message.where(receiver: @user)
-		@messages_sent = Message.where(sender: @user)
+		@messages_received = Message.where(receiver_id: @user)
+		@messages_sent = Message.where(sender_id: @user)
 	end
 
 	private 
